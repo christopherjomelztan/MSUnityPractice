17d2508496f87acc95c7e4c6a83b4660e07375f0
christopherjomelztan <christopherjomelztan@gmail.com>
2017-04-12 14:05:52 +0800

    Add project files.

---
+++ MSUnityPractice/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.Unity;
+
+namespace MSUnityPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IUnityContainer container = new UnityContainer();
+            ContainerMagic.RegisterElements(container);
+
+            IBattery battery = container.Resolve<IBattery>();
+            Console.WriteLine(battery.SerialNumber());
+
+            Dial dial = container.Resolve<Dial>();
+            Console.WriteLine(dial.DialType());
+
+            ITuner tuner = container.Resolve<ITuner>();
+            IRadio radio = container.Resolve<IRadio>(new ParameterOverride("radioBattery", battery),
+                new ParameterOverride("radioTuner", tuner),
+                new ParameterOverride("radioName", "BrokenRadio"));
+            radio.Start();
+            Console.ReadKey();
+        }
+    }
+}
